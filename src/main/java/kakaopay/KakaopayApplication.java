8131334f935d6b43f4a7e17097514a3221578196
1500sheep <cjswotl6274@naver.com>
2018-10-12 22:29:37 +0900

    initial project

---
+++ src/main/java/kakaopay/KakaopayApplication.java
@@ -0,0 +1,12 @@
+package kakaopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaopayApplication.class, args);
+    }
+}
