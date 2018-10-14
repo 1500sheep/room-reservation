4ff27502417961e8646660bb87923dec0ebbc6e1
1500sheep <cjswotl6274@naver.com>
2018-10-14 13:43:16 +0900

    add unit Test, ReservationTest

---
+++ src/test/java/kakaopay/domain/ReservationTest.java
@@ -0,0 +1,17 @@
+package kakaopay.domain;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+
+public class ReservationTest {
+
+    private final static Logger logger = Logger.getGlobal();
+
+    @Test
+    public void validationTime() throws IOException {
+
+    }
+}
